8ca42d198dbf2e9550d87cae5c7a56cc3fdc7f88
Still <ssantazis907@gmail.com>
2025-08-11 14:00:54 +0400

    Add `ImportTransactionRequest` model and update `.gitignore` with expanded template

---
+++ BudgetTracker.Application/Models/Transaction/Requests/ImportTransactionRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace BudgetTracker.Application.Models.Transaction.Requests;
+
+public record ImportTransactionRequest(
+    int NameIndex,
+    int AmountIndex,
+    int DateIndex,
+    int DescriptionIndex,
+    bool IsFirstRowHeaders
+);
