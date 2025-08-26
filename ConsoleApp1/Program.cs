61a3a331fbeed4ee3452c56f32207fecc36accbf
Santazis <89458708+Santazis@users.noreply.github.com>
2025-08-26 16:29:26 +0400

    Delete ConsoleApp1 directory

--- ConsoleApp1/Program.cs
+++
@@ -1,6 +0,0 @@
-﻿using BenchmarkDotNet.Running;
-using BudgetTracker.Infrastructure.Database;
-using ConsoleApp1;
-using Microsoft.Extensions.DependencyInjection;
-
-BenchmarkRunner.Run<EfCoreBenchmarks>();
