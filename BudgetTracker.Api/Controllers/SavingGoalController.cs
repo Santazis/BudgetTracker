7ce5968e2e4e8cfb9f87b5dbac789660231b62bd
Still <ssantazis907@gmail.com>
2025-09-01 18:32:56 +0400

    Created Saving Goal service

---
+++ BudgetTracker.Api/Controllers/SavingGoalController.cs
@@ -0,0 +1,42 @@
+﻿using BudgetTracker.Application.Interfaces;
+using BudgetTracker.Application.Models;
+using BudgetTracker.Application.Models.SavingGoal.Requests;
+using BudgetTracker.Extensions;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BudgetTracker.Controllers;
+[ApiController]
+[Route("api/goals")]
+public class SavingGoalController :ControllerBase
+{
+    private readonly ISavingGoalService _savingGoalService;
+    public Guid? UserId => User.GetUserId();
+    public SavingGoalController(ISavingGoalService savingGoalService)
+    {
+        _savingGoalService = savingGoalService;
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> CreateAsync(CreateSavingGoal request, CancellationToken cancellation)
+    {
+        if (UserId is null) return Unauthorized();
+        var result = await _savingGoalService.CreateAsync(request, UserId.Value, cancellation);
+        if (result.IsFailure)
+        {
+            return BadRequest(result.Error);
+        }
+        return Ok();
+    }
+    [HttpGet("{id:guid}")]
+    [ProducesResponseType<SavingGoalDto>(200)]
+    public async Task<IActionResult> GetByIdAsync([FromRoute] Guid id, CancellationToken cancellation)
+    {
+        if (UserId is null) return Unauthorized();
+        var result = await _savingGoalService.GetByIdAsync(id, UserId.Value, cancellation);
+        if (result.IsFailure)
+        {
+            return BadRequest(result.Error);
+        }
+        return Ok(result.Value);
+    }
+}
---
+++ BudgetTracker.Infrastructure/Repositories/SavingGoalRepository.cs
@@ -0,0 +1,28 @@
+﻿using BudgetTracker.Domain.Models.SavingGoal;
+using BudgetTracker.Domain.Repositories;
+using BudgetTracker.Infrastructure.Database;
+using Microsoft.EntityFrameworkCore;
+
+namespace BudgetTracker.Infrastructure.Repositories;
+
+public class SavingGoalRepository : ISavingGoalRepository
+{
+    private readonly ApplicationDbContext _context;
+
+    public SavingGoalRepository(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task CreateAsync(SavingGoal savingGoal, CancellationToken cancellation)
+    {
+        await _context.SavingGoals.AddAsync(savingGoal, cancellation);
+    }
+
+    public async Task<SavingGoal?> GetByIdAsync(Guid id, Guid userId, CancellationToken cancellation)
+    {
+        return await _context.SavingGoals
+            .Include(t=> t.Transactions)
+            .FirstOrDefaultAsync(t=> t.Id == id && t.UserId == userId, cancellation);
+    }
+}
